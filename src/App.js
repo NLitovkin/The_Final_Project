3030a789ea65d46f458857c770af9de78907ac86
NLitovkin <nlitovkin26@gmail.com>
2023-01-09 10:18:28 +0200

    started semantic structure

--- src/App.js
+++ src/App.js
@@ -9,3 +9,33 @@ function App() {
 }
 
 export default App;
+
+<header>
+  <img></img>
+</header>
+<nav>
+  <ul>
+    <li></li>
+    <li></li>
+    <li></li>
+    <li></li>
+    <li></li>
+    <li></li>
+  </ul>
+</nav>
+<main>
+  <h1></h1>
+  <h2></h2>
+  <article>
+    </article>
+  <img></img>
+  <button>
+    <label></label>
+  </button>
+  <h1></h1>
+  <button>
+    <label></label>
+  </button>
+</main>
+<footer>
+  </footer>
